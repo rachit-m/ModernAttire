8bf608ca74d26bd800e33dd568b344706041a64b
Chandan Pandey <chandan.dv682@gmail.com>
2022-09-30 09:04:14 +0530

    Add files via upload

---
+++ script/scripts.js
@@ -0,0 +1,60 @@
+
+// Accordion
+document.getElementById("fabric_nav").onclick = function (event) {
+  var target = event.target || event.srcElement;
+  var type =
+    target.innerText || target.textContent || target.parentElement.innerText;
+  var x = document.getElementById("expand_" + type);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+};
+
+function myAccFunc() {
+  var x = document.getElementById("demoAcc");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+//Facebook Pixel Code
+!(function (f, b, e, v, n, t, s) {
+  if (f.fbq) return;
+  n = f.fbq = function () {
+    n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+  };
+  if (!f._fbq) f._fbq = n;
+  n.push = n;
+  n.loaded = !0;
+  n.version = "2.0";
+  n.queue = [];
+  t = b.createElement(e);
+  t.async = !0;
+  t.src = v;
+  s = b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t, s);
+})(
+  window,
+  document,
+  "script",
+  "https://connect.facebook.net/en_US/fbevents.js"
+);
+fbq("init", "837966090480815");
+fbq("track", "PageView");
+
+
